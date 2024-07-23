96965cc1ca5c7abbe4affdd96f5f6b55ed7f78fc
taeining <taeining00@gmail.com>
2024-07-23 16:50:59 +0900

    [01] 프로그래머스 -  가장 큰 수

---
+++ CodingTestSolutions/src/basics/Solution_01.java
@@ -0,0 +1,25 @@
+package basics;
+import java.util.*;
+public class Solution_01 {
+    public String solution(int[] numbers) {
+
+        String[] arr = new String[numbers.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuffer answer = new StringBuffer();
+        for(String num : arr){
+            answer.append(num);
+        }
+
+        return answer.toString();
+    }
+}
